9dd36d3288e76e0b7c81ffd2dcd3a3ee75627064
a99os <abbosashirboev@gmail.com>
2022-12-02 09:12:57 +0500

    add prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
